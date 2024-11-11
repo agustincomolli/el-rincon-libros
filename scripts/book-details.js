68bae631c8f017550f8a4b696e3a43cab4d88cea
Agustín Comolli <agustincomolli@gmail.com>
2024-11-11 12:41:59 -0300

    feat: Add error 404 page
    feat: add pagination
    
    load book details

---
+++ scripts/book-details.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+    loadData();
+});
+
+/**
+ * Carga los datos del libro en la página
+ *
+ * Obtiene el ID del libro de la URL, busca el libro en el archivo books.json
+ * y, si lo encuentra, carga sus datos en la página
+ *
+ * @returns {undefined}
+ */
+function loadData() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const bookId = urlParams.get("id");
+
+    fetch("/data/books.json")
+        .then(response => response.json())
+        .then(data => {
+            const book = data.find(book => book.id === bookId);
+            if (book) {
+                // Carga los datos del libro en la página
+                document.querySelector("#book-title").textContent = book.title;
+                document.querySelector(".book-author").textContent = book.author;
+                document.querySelector(".book-description").textContent = book.description;
+                document.querySelector(".book-price").textContent = book.price;
+                document.querySelector(".book-image").setAttribute("src", `../assets/images/${book.cover}`);
+                document.querySelector(".book-image").setAttribute("alt", `Portada de ${book.title}`);
+                document.querySelector(".book-image").setAttribute("title", `Portada de ${book.title}`);
+                document.querySelector("#book-genre").textContent = `Género:  ${book.genre}`;
+                document.querySelector("#book-year").textContent = `Publicado en: ${book.publicationDate}`;
+                document.querySelector("#book-pages").textContent = `Páginas: ${book.pages}`;
+                document.querySelector("#book-publisher").textContent = `Editorial: ${book.publisher}`;
+                document.querySelector("#book-isbn").textContent = `ISBN: ${book.isbn}`;
+            } else {
+                // Libro no encontrado
+                console.error("Libro no encontrado");
+                window.location.href = "../pages/error-404.html";
+            }
+        })
+        .catch(error => console.error("Error:", error));
+};
--- scripts/books.js
+++ scripts/books.js
@@ -3,52 +3,104 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 /**
- * Carga los datos de los libros en el contenedor
- * 
- * Llena el contenedor .books-container con la informaci n de los libros
- * obtenida desde el archivo books.json
- * 
+ * Carga los libros en la página
+ *
+ * @param {number} page - Página que se quiere cargar (opcional, por defecto 1)
+ *
  * @returns {undefined}
  */
-function loadData() {
+function loadData(page = 1) {
+    const booksPerPage = 15;
+    const startIndex = (page - 1) * booksPerPage;
+    const endIndex = startIndex + booksPerPage;
+
     fetch('/data/books.json')
         .then(response => response.json())
         .then(data => {
             const booksContainer = document.querySelector('.books-container');
-            data.forEach(book => {
-                const bookElement = document.createElement('div');
-                bookElement.className = 'book';
+            booksContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar los nuevos libros
 
-                const titleElement = document.createElement('h3');
-                titleElement.className = 'book-title';
-                titleElement.textContent = book.title;
-                bookElement.appendChild(titleElement);
-
-                const authorElement = document.createElement('h4');
-                authorElement.className = 'book-author';
-                authorElement.textContent = book.author;
-                bookElement.appendChild(authorElement);
-
-                const coverElement = document.createElement('a');
-                coverElement.href = `./book-details.html?id=${book.id}`;
-                coverElement.dataset.bookId = book.id;
-                coverElement.innerHTML = `<img class="book-cover" src="../assets/images/${book.cover}" alt="Portada de ${book.title}" height="300px" title="${book.title}">`;
-                bookElement.appendChild(coverElement);
-
-                const priceElement = document.createElement('p');
-                priceElement.className = 'book-price';
-                priceElement.textContent = book.price;
-                bookElement.appendChild(priceElement);
-
-                const addToCartElement = document.createElement('a');
-                addToCartElement.className = 'add-to-cart-button';
-                addToCartElement.href = '#';
-                addToCartElement.title = 'Agregar al carrito';
-                addToCartElement.innerHTML = '<i class=\'bx bx-cart\'></i>';
-                bookElement.appendChild(addToCartElement);
-
+            for (let i = startIndex; i < endIndex && i < data.length; i++) {
+                const book = data[i];
+                const bookElement = createBookElement(book);
                 booksContainer.appendChild(bookElement);
-            });
+            }
+
+            updatePagination(page, Math.ceil(data.length / booksPerPage));
         })
         .catch(error => console.error('Error:', error));
+}
+
+/**
+ * Crea un elemento HTML que representa un libro
+ * 
+ * @param {object} book - Objeto con la informaci n del libro
+ * @returns {HTMLElement} - Elemento HTML que representa el libro
+ */
+function createBookElement(book) {
+    const bookElement = document.createElement('div');
+    bookElement.className = 'book';
+
+    const titleElement = document.createElement('h3');
+    titleElement.className = 'book-title';
+    titleElement.textContent = book.title;
+    bookElement.appendChild(titleElement);
+
+    const authorElement = document.createElement('h4');
+    authorElement.className = 'book-author';
+    authorElement.textContent = book.author;
+    bookElement.appendChild(authorElement);
+
+    const coverElement = document.createElement('a');
+    coverElement.href = `./book-details.html?id=${book.id}`;
+    coverElement.dataset.bookId = book.id;
+    coverElement.innerHTML = `<img class="book-cover" src="../assets/images/${book.cover}" alt="Portada de ${book.title}" height="300px" title="${book.title}">`;
+    bookElement.appendChild(coverElement);
+
+    const priceElement = document.createElement('p');
+    priceElement.className = 'book-price';
+    priceElement.textContent = book.price;
+    bookElement.appendChild(priceElement);
+
+    const addToCartElement = document.createElement('a');
+    addToCartElement.className = 'add-to-cart-button';
+    addToCartElement.href = '#';
+    addToCartElement.title = 'Agregar al carrito';
+    addToCartElement.innerHTML = '<i class=\'bx bx-cart\'></i>';
+    bookElement.appendChild(addToCartElement);
+
+    return bookElement;
+}
+
+/**
+ * Actualiza la paginación de la sección de libros
+ *
+ * @param {number} currentPage - Página actual
+ * @param {number} totalPages - Total de páginas
+ *
+ * @returns {undefined}
+ */
+function updatePagination(currentPage, totalPages) {
+    const paginationContainer = document.querySelector('.pagination ul');
+    paginationContainer.innerHTML = '';
+
+    // Agregar el botón "Anterior"
+    const prevButton = document.createElement('li');
+    prevButton.innerHTML = `<a href="#" class="prev" title="Anterior"><i class='bx bxs-chevron-left'></i></a>`;
+    prevButton.querySelector('a').addEventListener('click', () => loadData(currentPage - 1));
+    paginationContainer.appendChild(prevButton);
+
+    // Agregar los números de página
+    for (let i = 1; i <= totalPages; i++) {
+        const pageButton = document.createElement('li');
+        pageButton.innerHTML = `<a href="#" class="page-numbers${currentPage === i ? ' active' : ''}">${i}</a>`;
+        pageButton.querySelector('a').addEventListener('click', () => loadData(i));
+        paginationContainer.appendChild(pageButton);
+    }
+
+    // Agregar el botón "Siguiente"
+    const nextButton = document.createElement('li');
+    nextButton.innerHTML = `<a href="#" class="next" title="Siguiente"><i class='bx bxs-chevron-right'></i></a>`;
+    nextButton.querySelector('a').addEventListener('click', () => loadData(currentPage + 1));
+    paginationContainer.appendChild(nextButton);
 }
